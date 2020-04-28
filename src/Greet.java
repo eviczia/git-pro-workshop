d71647e6b17fd3e7014cb4587f87de23c5cbc364
banatihajnalka <banati.hajnalka@gmail.com>
2020-04-28 10:21:21 +0200

    a1

---
+++ src/Greet.java
@@ -0,0 +1,11 @@
+public class Greet {
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+
+    public static String greet() {
+        return "Hajni";
+    }
+
+}
